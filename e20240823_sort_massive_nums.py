c368e10cb78a97d87d88596fb2044e45c4991879
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-07-23 23:45:11 +0500

    Ранее решал не правильно. По условиям - необходимо использовать алгоритм Флойдовой Чарепахи.

--- e20240823_sort_massive_nums.py
+++ e20240823_sort_massive_nums.py
@@ -9,7 +9,8 @@
 Автор: ЕВгений Петров
 e-mail: epoy74@gmail.com
 
-Сайт задачи: https://leetcode.com/problems/find-all-duplicates-in-an-array/?envType=daily-question&envId=2024-07-07
+Сайт задачи: https://leetcode.com/problems/find-all-duplicates-in-an-array/
+?envType=daily-question&envId=2024-07-07
 """
 
 from typing import List
@@ -29,10 +30,10 @@ class Solution:
         :return: Массив дубликатов
         """
         out_list: List[int] = []
-        for i in range(len(nums)):
-            count_nums: int = nums.count(nums[i])
-            if count_nums > 1 and nums[i] not in out_list:
-                out_list.append(int(nums[i]))
+        for k in range(len(nums)):
+            count_nums: int = nums.count(nums[k])
+            if count_nums > 1 and nums[k] not in out_list:
+                out_list.append(int(nums[k]))
         return out_list[0]
 
     def find_duplicate_new(self, nums: List[int]) -> int:
@@ -43,14 +44,81 @@ class Solution:
         """
         out_list: List[int] = []
         repeated_int: int = 0
-        for i in range(len(nums)):
+        for l in range(len(nums)):
             count_nums: int = nums.count(nums[i])
-            if count_nums > 1 and nums[i] not in out_list:
-                # out_list.append(int(nums[i]))
-                repeated_int = int(nums[i])
+            if count_nums > 1 and nums[l] not in out_list:
+                # out_list.append(int(nums[l]))
+                repeated_int = int(nums[l])
         return repeated_int
+
+    def find_duplicate_new_2(self, nums: List[int]) -> int:
+        """
+        LeetCode этот ответ принял.... И почему-то помеянлись условия задачи...
+        :param nums:
+        :return:
+        """
+        nums.sort()
+        for j in range(len(nums)):
+            if j+1!= len(nums):
+                if nums[j] == nums[j+1] :
+                    return nums[j]
+
+    def find_duplicate_new_3(self, nums: List[int]) -> int:
+        """
+        LeetCode этот ответ принял.... И почему-то помеянлись условия задачи...
+        :param nums:
+        :return:
+        """
+
+        # num = enumerate(nums)
+        for j in range(len(nums)):
+            if nums.count(nums[j]) > 1:
+                return nums[j]
+
+
+    def find_duplicate_new_4(self, nums: List[int]) -> int:
+        """
+        Корректно по условиям задачи использовать алгоритм Флойдовой черепахи
+        :param nums:
+        :return:
+        """
+
+        tortoise = hare = nums[0]
+        while True:
+            hare = nums[nums[hare]]
+            tortoise = nums[tortoise]
+            if hare == tortoise:
+                break
+        print(hare)
+
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+        print(hare)
+        return hare
+
 
 
 if __name__ == "__main__":
+    # input_list: List[int] = [0]
+    with open("20240723_num_list.txt","r") as f_list_int:
+        # for line in f_list_int:
+            #input_list.append([int(x) for x in line.split(",")])
+        # Читаем содержимое файла и разделяем строки по запятым
+        lines = f_list_int.read().split(",")
+        # Преобразуем строки в числа
+        input_list: List[int] = [int(line) for line in lines]
+
+    # for i in range(len(input_list)):
+    #     print(input_list[i])
+
+    input_list.sort()
+    for i in range(5):
+        print("__________________________________________________________________________________")
+    # print(len(input_list))
+    # for i in range(len(input_list)):
+    #     print(input_list[i])
+
     my_nums = Solution()
-    print(my_nums.find_duplicate_new( [1,3,4,2,2]))
+    print(my_nums.find_duplicate_new_4(input_list))
